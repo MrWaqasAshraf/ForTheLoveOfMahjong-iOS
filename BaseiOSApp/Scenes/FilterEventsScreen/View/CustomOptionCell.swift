cb3a37063d285f7c33b269389176b09f5bcedffe
waqas <waqas.f619@gmail.com>
2025-10-02 00:57:26 +0500

    assa

--- BaseiOSApp/Scenes/FilterEventsScreen/View/CustomOptionCell.swift
+++ BaseiOSApp/Scenes/FilterEventsScreen/View/CustomOptionCell.swift
@@ -22,29 +22,31 @@ class CustomOptionCell: UICollectionViewCell {
     
     func configureCell(data: CustomOptionModel?, forCategory: Bool = false) {
         titleLbl.text = data?.title
-        titleLbl.textColor = data?.isSelected == true ? .white : .label
+        
         containerView.backgroundColor = data?.isSelected == true ? .clr_primary : .clr_gray_3
         
         if forCategory {
             checkmarkView.isHidden = !(data?.isSelected == true)
+            titleLbl.textColor = /*data?.isSelected == true ?*/ .white /*: .label*/
             if let category = data?.title {
                 switch category {
                 case "American":
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_american : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_american /*: .clr_gray_3*/
                 case "Chinese":
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_chinese : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_chinese /*: .clr_gray_3*/
                 case "Hong Kong":
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_yellow : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_yellow /*: .clr_gray_3*/
                 case "Riichi":
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_riichie : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_riichie /*: .clr_gray_3*/
                 case "Wright Patterson":
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_wright_patterson : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_wright_patterson /*: .clr_gray_3*/
                 default:
-                    containerView.backgroundColor = data?.isSelected == true ? .clr_primary : .clr_gray_3
+                    containerView.backgroundColor = /*data?.isSelected == true ?*/ .clr_primary /*: .clr_gray_3*/
                 }
             }
         }
         else {
+            titleLbl.textColor = data?.isSelected == true ? .white : .label
             checkmarkView.isHidden = true
         }
         
